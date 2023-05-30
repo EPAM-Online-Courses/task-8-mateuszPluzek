ff257ce306af8d31048a5b7534b407213dab6834
mateusz.pluzek <mateusz.pluz@gmail.com>
2023-05-30 17:09:00 +0200

    Task 2/ final

--- src/test/java/efs/task/unittests/PlannerTest.java
+++ src/test/java/efs/task/unittests/PlannerTest.java
@@ -4,34 +4,35 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlannerTest {
     public Planner plan;
-
-
     @BeforeEach
     public void setObject(){
         plan = new Planner();
     }
-
     @ParameterizedTest
     @EnumSource(ActivityLevel.class)
     void shouldReturnCorrectValues_whenGivenEnumValues(ActivityLevel activity) {
-        int calculatedValue = plan.calculateDailyCaloriesDemand(TestConstants.TEST_USER, activity);
+        User user = TestConstants.TEST_USER;
+
+        int calculatedValue = plan.calculateDailyCaloriesDemand(user, activity);
 
         assertTrue(TestConstants.CALORIES_ON_ACTIVITY_LEVEL.get(activity) == calculatedValue);
     }
-
     @Test
     void shouldReturnCorrectDailyIntake_whenGivenTestUser() {
+        DailyIntake expectedIntake = TestConstants.TEST_USER_DAILY_INTAKE;
+
         DailyIntake calculatedIntake = plan.calculateDailyIntake(TestConstants.TEST_USER);
-        assertTrue(calculatedIntake.getCalories() == TestConstants.TEST_USER_DAILY_INTAKE.getCalories() &&
-        calculatedIntake.getProtein() == TestConstants.TEST_USER_DAILY_INTAKE.getProtein() &&
-        calculatedIntake.getFat() == TestConstants.TEST_USER_DAILY_INTAKE.getFat() &&
-        calculatedIntake.getCarbohydrate() == TestConstants.TEST_USER_DAILY_INTAKE.getCarbohydrate());
+
+        assertEquals(calculatedIntake.getCalories(), expectedIntake.getCalories());
+        assertEquals(calculatedIntake.getProtein(), expectedIntake.getProtein());
+        assertEquals(calculatedIntake.getFat(), expectedIntake.getFat());
+        assertEquals(calculatedIntake.getCarbohydrate(), expectedIntake.getCarbohydrate());
     }
 
 
