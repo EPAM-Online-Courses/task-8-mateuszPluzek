7ee0add8eb5aa03629aa6d622dc565832d60944e
mateusz.pluzek <mateusz.pluz@gmail.com>
2023-05-30 11:48:24 +0200

    Task 1

--- src/test/java/efs/task/unittests/FitCalculatorTest.java
+++ src/test/java/efs/task/unittests/FitCalculatorTest.java
@@ -1,8 +1,12 @@
 package efs.task.unittests;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FitCalculatorTest {
 
@@ -18,4 +22,55 @@ class FitCalculatorTest {
         //then
         assertTrue(recommended);
     }
+
+    @Test
+    void shouldReturnFalse_whenDietNotRecommended() {
+        double weight = 69.5;
+        double height = 1.72;
+
+        boolean recommended = FitCalculator.isBMICorrect(weight, height);
+
+        assertFalse(recommended);
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentException_whenWrongArgument() {
+        double weight = 72.3;
+        double height = 0.0;
+        try {
+            boolean recommended = FitCalculator.isBMICorrect(weight, height);
+            fail("expected exception hasn't occured\n");
+        }
+        catch(IllegalArgumentException e) {
+            ;
+        }
+    }
+
+    @ParameterizedTest(name = "weight = {0}")
+    @ValueSource(doubles = {90.3, 86.3, 82.4})
+    void shouldReturnTrue_whenGivenValidWeights(double arg) {
+        double height = 1.80;
+        boolean recommended = FitCalculator.isBMICorrect(arg, height);
+        assertTrue(recommended);
+    }
+
+    @ParameterizedTest(name = "weight = {0}, height = {1}")
+    @CsvSource({
+        "68.2, 1.72",
+        "74.9, 1.82",
+        "53.7, 1.65"
+    })
+    void shouldReturnFalse_whenGivenInvalidPairs(double weight, double height) {
+        boolean recommended = FitCalculator.isBMICorrect(weight, height);
+        assertFalse(recommended);
+    }
+
+    @ParameterizedTest(name = "weight = {0}, height = {1}")
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1)
+    void shouldReturnTrue_whenGivenDataFromFile(double weight, double height) {
+        boolean recommended = FitCalculator.isBMICorrect(weight, height);
+        assertFalse(recommended);
+    }
+
+
 }
